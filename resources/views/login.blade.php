74a3f74f2bdda4e6f96471fada8ab3dd2d64746e
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-05-13 19:30:49 +0300

    sessionSetup

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,18 @@
 <div class='container'>
     <div class="row">
         <div class='col-sm-4 offset-sm-4'>
-        <form>
+        <form action='login' method='POST'>
             <div class="mb-3">
+                @csrf
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" name='email' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 <div id="emailHelp" class="form-text">We'll never share your information with third parties.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" name='password' class="form-control" id="exampleInputPassword1">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Login</button>
         </form>
         </div>
     </div>
